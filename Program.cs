5d0062cecb061436f3617a563d9453ad8cf100a9
Kendra Egert <mskendrae@gmail.com>
2016-09-16 09:47:46 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sentence = "some line with text";
+            int sentLeng = sentence.Length;
+            string[] sent = sentence.Split(' '); //add words into an array
+            string longword = "";
+
+            foreach (string word in sent)
+            {
+                int wordLength = word.Length;
+                int longWordLength = longword.Length;
+                if (wordLength > longWordLength)
+                {
+                    longWordLength = wordLength;
+                    longword = word;
+                } 
+
+            }
+            Console.WriteLine(longword);
+
+
+
+        }
+
+       
+    }
+
+}
+
+
